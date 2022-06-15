15d433c28fc585b7a9ea4ca27220f18795c4fef9
rechu12 <101204628+rechu12@users.noreply.github.com>
2022-06-15 12:26:18 -0400

    Update envs.groovy

--- envs.groovy
+++ envs.groovy
@@ -1,4 +1,4 @@
-env.SET_URL="test0303a.ninjasleep.com"
+env.SET_URL="stage-0601.ninjasleep.com"
 env.BRIGHTREE_SYNC_PROD="BRIGHTREE_SYNC_PROD"
 env.BUILD_TRIGGER_BY = "${currentBuild.getBuildCauses()[0].userId}"
 env.TRACKER="TRACKER"
